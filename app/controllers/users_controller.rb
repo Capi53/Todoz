3bf0edfbd432d8f732678cf2878ca06ec054528e
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-05 14:26:51 +0900

    navbarっぽいやつ追加した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  layout "boards_header"
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render 'new'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:id, :name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users/:id', to:'users#show'
+  # get '/users/:id', to:'users#show'
+  # root "users#show"
+  get '/users/:id' => 'users#show'
 end
