fd4213b964df4f30ed3ccbd43cd740a75825a55c
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-06 22:34:53 +0900

    動くようになった〜

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/users/:id', to:'users#show'
   # root "users#show"
-  # get '/users/:id' => 'users#show'
+  get '/user/:id' => 'users#show'
 end
