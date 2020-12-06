09315e19f9c329996ebd5e5d90187e70118da779
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-06 23:42:39 +0900

    ボタンを追加できるようにした

--- app/assets/javascripts/board_operations.js
+++ app/assets/javascripts/board_operations.js
@@ -3,3 +3,13 @@
 //     $(elid).append('div');
 
 // });
+$(document).on("click", ".add-btn", function () {
+    // var num = Number($(this).text().replace("+add list", ""));
+    // if (num==NaN){
+    //     var num = Number($(this).text().replace("list", ""));
+    // };
+    // $(".btn-area").append(html);
+    var num = $('.add-btn').length;
+    var html = '<button type="button" class="add-btn">list' + (num + 1) + '</button>';
+    $('.adder_btn').before(html);
+});
