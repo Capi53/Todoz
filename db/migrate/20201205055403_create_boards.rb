5b876ae61df0d43d41cca6b76a94b686cdff7354
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-13 22:10:26 +0900

    手で直した

--- db/migrate/20201205055403_create_boards.rb
+++ db/migrate/20201205055403_create_boards.rb
@@ -2,7 +2,7 @@ class CreateBoards < ActiveRecord::Migration[5.2]
   def change
     create_table :boards do |t|
       t.string :name
-
+      t.integer :created_by
       t.timestamps
     end
   end
